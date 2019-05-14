5848099a7299380b7c5b3edf37faf6f57c40e48c
Thomas Huber <huber.thomas@outlook.com>
2019-05-14 13:48:13 +0200

    from hed

--- examples/ced/solve.py
+++ examples/ced/solve.py
@@ -1,20 +1,70 @@
+# -*- coding: utf-8 -*-
 from __future__ import division
 import numpy as np
 import sys
-caffe_root = '../../' 
+import argparse
+from os.path import isfile, join, isdir
+import logging
+from helpers import correctFilePath
+
+caffe_root = '../../'
 sys.path.insert(0, caffe_root + 'python')
 import caffe
 
+def checkInputParameter(args):
+  '''
+  @brief Checks the input parameter.
+  @param args Input arguments.
+  @return Validated arguments in the input structure.
+  '''
+  if not isfile(args.baseWeights):
+    raise IOError('Model "%s" does not exist.' % (args.baseWeights))
 
-base_weights = '../../../model/hed_vgg16.caffemodel'
+  if not isfile(args.netProto):
+    raise IOError('Network "%s" does not exist.' % (args.netProto))
 
-# init
-caffe.set_mode_gpu()
-caffe.set_device()
+  return args
 
-solver = caffe.SGDSolver('solver.prototxt')
+def process(args):
+    '''
+    @brief Process model solving.
+    @param args Input arguments.
+    '''
+    if args.gpu:
+        caffe.set_mode_gpu()
+        caffe.set_device(0)
+    else:
+        caffe.set_mode_cpu()
 
-# copy base weights for fine-tuning
-solver.net.copy_from(base_weights)
+    solver = caffe.SGDSolver(args.netProto)
 
-solver.step()
+    # copy base weights for fine-tuning
+    solver.net.copy_from(args.baseWeights)
+    solver.step()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Test images on Deep Crisp Boundaries algorithm.')
+    parser.add_argument('--gpu', default=False,
+                        action='store_true', help='Enable GPU mode.')
+    parser.add_argument('--baseWeights', type=str,
+                        default='hed_vgg16.caffemodel', help="Path to the Caffe model file.")
+    parser.add_argument('--netProto', type=str, default='solver.prototxt',
+                        help='Path to the proto network file.')
+
+    try:
+      args = checkInputParameter(parser.parse_args())
+
+      process(args)
+    except Exception as e:
+      logging.error(e)
+
+
+if __name__ == '__main__':
+  logging.basicConfig(
+      format='%(asctime)s %(levelname)s:\t%(message)s', level=logging.DEBUG)
+  print('#####################')
+  print('Deep Crisp Boundaries')
+  print('#####################')
+  main()
